52575d83ea93be932fadb22b720c02dc8843f59f
chanwihad <dgunnerxxx@gmail.com>
2020-11-05 17:07:13 +0700

    initialization commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Laravel\Lumen\Routing\Controller as BaseController;
+use App\Models\Book;
+
+class Controller extends BaseController
+{
+    //
+    public function index()
+    {
+        return Book::all();
+    }
+    public function show($id)
+    {
+        if ($book = Book::find($id)) {
+            return $book;
+        } else {
+            return response('Book Not Found', 404);
+        }
+    }
+}
